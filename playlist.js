c809cd6df1ddfca88f2c4f34260c3c854689cdb3
Karthik V K <101323446+karthik-v-k@users.noreply.github.com>
2025-10-12 21:08:56 +0530

    created js and put array in playlist

---
+++ playlist.js
@@ -0,0 +1,12 @@
+const playlist = [
+    {
+        id : "1",
+        image : "songs-images/1.jpg",
+        name : "Life Goes On",
+        artistName : "Oliver Tree",
+        albumName : "Ugly is Beautiful : Shorter, thicker & uglier",
+        dateAdded : "Oct 23, 2022",
+        duration : "2:41"
+    }
+];
+
